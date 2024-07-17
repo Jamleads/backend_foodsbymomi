9c3abe28e912127cfecfbd4828cabc63122a915c
manlikeHB <hbyusuf40@gmail.com>
2024-07-17 18:39:35 +0100

    Handle bearer auth

--- controller/authController.js
+++ controller/authController.js
@@ -203,7 +203,10 @@ exports.login = catchAsync(async (req, res, next) => {
 exports.protect = catchAsync(async (req, res, next) => {
   // Get token
   let token;
-  if (req.cookies.jwt) {
+
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
   }
 
